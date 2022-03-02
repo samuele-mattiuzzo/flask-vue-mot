4bb4a10a65e660252c7500a6fcb27be83eb6b8e4
Samuele Mattiuzzo <samumatt@gmail.com>
2022-03-02 20:29:41 +0000

    Showing errors in the client

--- client/src/components/Mot.vue
+++ client/src/components/Mot.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="container" >
-      <p>Reg: {{ details.registration }}</p>
-      <p>Make: {{ details.make }}</p>
-      <p>Model: {{ details.model }}</p>
-      <p>Next MOT due by: {{ details.motTests[0].expiryDate }}</p>
-      <p>Past MOTs: {{ details.motTests.length }}</p>
-      <ul v-for="mot in details.motTests" :key="mot.motTestNumber">
-          <li>{{ mot.motTestNumber }}
-              with status {{ mot.testResult }} on {{ mot.completedDate }}
-          </li>
-      </ul>
+      <h1 v-if="error">{{ error }}</h1>
+      <div v-if="details">
+        <p>Reg: {{ details.registration }}</p>
+        <p>Make: {{ details.make }}</p>
+        <p>Model: {{ details.model }}</p>
+        <p>Next MOT due by: {{ details.motTests[0].expiryDate }}</p>
+        <p>Past MOTs: {{ details.motTests.length }}</p>
+        <ul v-for="mot in details.motTests" :key="mot.motTestNumber">
+            <li>{{ mot.motTestNumber }}
+                with status {{ mot.testResult }} on {{ mot.completedDate }}
+            </li>
+        </ul>
+      </div>
   </div>
 </template>
 <script>
@@ -20,6 +23,7 @@ export default {
   data() {
     return {
       details: '',
+      error: '',
     };
   },
   methods: {
@@ -28,7 +32,12 @@ export default {
       const path = `http://localhost:5000/mot?reg=${reg}`;
       axios.get(path)
         .then((res) => {
-          this.details = res.data;
+          console.log(res.data);
+          if (res.data.error) {
+            this.error = res.data.error;
+          } else {
+            this.details = res.data;
+          }
         })
         .catch((error) => {
           // eslint-disable-next-line
